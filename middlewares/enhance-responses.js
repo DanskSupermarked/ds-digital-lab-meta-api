64b4402b2d6f41b0ff574f1b66ef33e54b0d1f91
Jakob Viskum Damgaard <jakob.viskum.damgaard@gmail.com>
2016-03-06 22:46:28 +0100

    Use markdown in responses

--- middlewares/enhance-responses.js
+++ middlewares/enhance-responses.js
@@ -4,6 +4,18 @@
 
 var timeAgo = require('epoch-to-timeago').timeAgo;
 var wordCount = require('word-count');
+var marked = require('marked');
+
+var renderer = new marked.Renderer();
+
+renderer.heading = function(text, level) {
+  return '<p class="h' + level + '"">' + text + '</p>';
+};
+
+marked.setOptions({
+  renderer: renderer,
+  sanitize: true
+});
 
 module.exports = function(req, res, next) {
   if (req.payload && req.payload.responses && req.query.raw === undefined) {
@@ -15,6 +27,9 @@ module.exports = function(req, res, next) {
       var published = new Date(response.published).getTime();
       response.timeAgo = timeAgo(published, now);
 
+      // Markdown to html
+      response.html = marked(response.text);
+
       // Read time and excerpt
       var spaces = Array(41).join(' ');
       var textWithSpaces = response.text.replace(/\r?\n/g, spaces);
@@ -23,6 +38,8 @@ module.exports = function(req, res, next) {
         response.excerpt = textWithSpaces.substr(0, spacePlacement) + '...';
         response.excerpt = response.excerpt.replace(new RegExp(spaces, 'g'), '\n');
 
+        response.excerpt = marked(response.excerpt);
+
         response.readTime = Math.ceil(wordCount(response.text) / 300);
         if (response.readTime > 1) {
           response.readTime += ' mins';
@@ -30,6 +47,7 @@ module.exports = function(req, res, next) {
           response.readTime += ' min';
         }
       }
+
     });
   }
 
