1afeb0c06b0df646c254dac4c327a2158ff14fd0
Jakob Viskum Damgaard <jakob.viskum.damgaard@gmail.com>
2016-10-06 09:57:46 +0200

    Updated dependencies

--- util/get-data-from-ghost-api.js
+++ util/get-data-from-ghost-api.js
@@ -3,7 +3,7 @@ const request = require('request');
 // Constants
 const GHOST_URL = process.env.GHOST_URL || 'http://localhost:3000';
 const BASE_URL = `${GHOST_URL}/ghost/api/v0.1/`;
-const CLIENT_SECRET = process.env.GHOST_CLIENT_SECRET || '18ba571f60c7';
+const CLIENT_SECRET = process.env.GHOST_CLIENT_SECRET || 'ea32de142546';
 
 module.exports = function getDataFromGhostAPI(resource, qs, callback) {
   callback = callback || function () {};
